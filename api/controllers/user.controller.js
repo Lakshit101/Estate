c30f3cfc4deeb06e1f7b04c78d22b1811f0d4570
Lakshit <Lakshitpaliwal9@gmail.com>
2026-09-06 19:19:59 +0530

    setup routes and controolers and utils

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,8 +2,5 @@ export const test = (req, res) => {
     res.send(`hwllo test`);
   };
 
-export const test2 = (req, res) => {
-    res.send(`bye bye`);
-  };
-
+ 
   
--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import dns from "dns";
 import userRouter from "./routes/user.routes.js"
+import authRouter from "./routes/auth.route.js"
 dotenv.config();
 
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const app = express();
+app.use(express.json());
 
 mongoose.connect(process.env.Mongo)
   .then(() => {
@@ -23,4 +25,14 @@ app.listen(3000, () => {
 
 
 app.use('/api/user',userRouter);
-app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
+ 
+app.use((err,req,res,next)=>{
+  const statusCode=err.statuscode || 500;
+  const mssg = err.mssg || "Internal server error";
+  return res.status(statusCode).json({
+    success:false,
+    statusCode,
+    message,
+  })
+})
